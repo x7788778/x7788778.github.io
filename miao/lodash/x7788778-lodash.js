50eca2229de1c1f076f26341ffff386376a8d20c
7788778 <156606672@qq.com>
2019-01-16 16:23:13 +0800

    lodash

--- miao/lodash/x7788778-lodash.js
+++ miao/lodash/x7788778-lodash.js
@@ -76,53 +76,7 @@ Returns
 //时间复杂度为n方，  
   return result
   },
-  baseDifference: function (array, values, iteratee, comparator) {
-    let includes = arrayIncludes
-    let isCommon = true
-    const result = []
-    const valuesLength = values.length
-
-    if (!array.length) {
-      return result
-    }
-    if (iteratee) {
-      values = map(values, (value) => iteratee(value))
-    }
-    if (comparator) {
-      // 有比较器时，改用支持比较器的arrayIncludesWith
-      includes = arrayIncludesWith
-      // 标识为不是普通比较
-      isCommon = false
-    }
-  // 当比较的数值长度超过200个时，启用缓存
-  else if (values.length >= LARGE_ARRAY_SIZE) {
-    includes = cacheHas
-    isCommon = false
-    values = new SetCache(values)
-  }
-  outer:
-  for (let value of array) {
-    // 迭代器最终值
-    const computed = iteratee == null ? value : iteratee(value)
-    value = (comparator || value !== 0) ? value : 0
-    // 普通比较，并且非NaN
-    if (isCommon && computed === computed) {
-      let valuesIndex = valuesLength
-      while (valuesIndex--) {
-        if (values[valuesIndex] === computed) {
-          // 如果有相等的元素，不必要继续遍历，回到最外层循环outer
-          continue outer
-        }
-      }
-      result.push(value)
-    }
-    // isCommon=false时，非普通比较，比较values里边是否存在computed
-    else if (!includes(values, computed, comparator)) {
-      result.push(value)
-    }
-  }
-  return result
-  },
+  
 // differenceBy = function (array, ...values) {
 //   // iteratee赋值为为最后一个参数
 //   let iteratee = last(values)
